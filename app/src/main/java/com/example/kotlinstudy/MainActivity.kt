c460e03e38088d0d666328633d278f9d14fa55c2
pachuho <blueblack969@naver.com>
2020-11-30 15:27:06 +0900

    TabLayout&ViewPager

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -1,32 +1,89 @@
 package com.example.kotlinstudy
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override var viewId: Int = R.layout.activity_main
+    override var toolbarId: Int? = R.id.toolbar
+    private lateinit var toast: Toast
+    private var backKeyPressedTime: Long = 200
+    private var mViewPager: ViewPager? = null
 
-        btn1.setOnClickListener{
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.view, OneFragment())
-                .commit()
+    @SuppressLint("ShowToast")
+    override fun onCreate() {
+        showActionBar()
+
+
+        mViewPager = findViewById(R.id.viewPager)
+        mViewPager!!.adapter = PagerAdapter(supportFragmentManager)
+        mViewPager!!.currentItem = 0
+
+        val tabLayout = findViewById<View>(R.id.tabs) as TabLayout
+        tabLayout.setupWithViewPager(mViewPager)
+
+
+        tabLayout.getTabAt(0)!!.text = "1"
+        tabLayout.getTabAt(1)!!.text = "2"
+        tabLayout.getTabAt(2)!!.text = "3"
+        tabLayout.getTabAt(3)!!.text = "4"
+
+        mViewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) = tab.select()
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+
+    }
+
+    override fun onBackPressed() {
+
+        if (System.currentTimeMillis() > backKeyPressedTime + 500) {
+            backKeyPressedTime = System.currentTimeMillis()
+            toast.show()
+            return
         }
 
-        btn2.setOnClickListener{
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.view, TwoFragment())
-                .commit()
+        if (System.currentTimeMillis() <= backKeyPressedTime + 500) {
+            toast.cancel()
+            this.finish()
+        }
+    }
 
+    inner class PagerAdapter(supportFragmentManager: FragmentManager) : FragmentStatePagerAdapter(supportFragmentManager,
+        BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        override fun getItem(position: Int): Fragment {
+
+            return when (position) {
+                0 ->
+                    TestFragment()
+                1 ->
+                    TestFragment()
+                2 ->
+                    TestFragment()
+                3 ->
+                    TestFragment()
+                else ->
+                    TestFragment()
+            }
+            throw IllegalStateException("position $position is invalid for this viewpager")
         }
 
-        btn3.setOnClickListener{
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.view, ThreeFragment())
-                .commit()
-        }
+        override fun getCount(): Int = 4
     }
 }
