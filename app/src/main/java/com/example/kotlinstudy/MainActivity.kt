8e88d17fc30c7f971cc86a7282eb3dfb7fe2b31d
pachuho <blueblack969@naver.com>
2020-11-30 14:37:03 +0900

    Fragment

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -5,23 +5,28 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var UserList = arrayListOf<User>(
-        User("심효근","shimhg02@naver.com","아령하세요잇!"),
-        User("박채연","asdf@naver.com","할말이 없다"),
-        User("박서연","qwerqr2@naver.com","ㄷ"),
-        User("박태욱","ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ@naver.com","ㅁㄴㅇㄹ"),
-        User("김민식","qwer2@naver.com","ㅇㅁㄴㄹ!"),
-        User("이소명","shㅇㄹ@naver.com","아령dsafsdf!"),
-        User("한규언","shiㅁㄴㅇㄹ@naver.com","afsdf!"),
-        User("정빈","shi@naver.com","ㅁㄴㅇㄹ"),
-        User("김태양","sㅁㄴㅇㄹㅁㅇㄴㄹaver.com","아ㅇ잇!")
-    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val Adapter = ListAdapter(this, UserList)
-        list_view.adapter = Adapter
-    }
+        btn1.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.view, OneFragment())
+                .commit()
+        }
 
+        btn2.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.view, TwoFragment())
+                .commit()
+
+        }
+
+        btn3.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.view, ThreeFragment())
+                .commit()
+        }
+    }
 }
