e3cc636326eb372bfc22444779a0732d79d9a0f3
park juho <blueblack96@dynamicmedia.co.kr>
2021-05-31 12:00:11 +0900

    coroutine test

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.kotlinstudy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.example.kotlinstudy.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val vm: MainViewModel by lazy { ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java) }
-
+    private val TAG = "test"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        GlobalScope.launch {
+            Log.d(TAG, "inner")
+        }
+        Log.d(TAG, "ex")
 
         binding.apply {
             viewModel = vm
             lifecycleOwner = this@MainActivity
         }
+
     }
 
     override fun onDestroy() {
